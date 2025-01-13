bc7fe1453fb2a905a8c3a1b15c46a087ba5f362e
chrVillon <chrovill@espol.edu.ec>
2025-01-13 09:12:54 -0500

    arreglo al apartado de opiniones

---
+++ src/credentials.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCHhpcKf4U_L-_EMJJ-7xoEQBqIXgzljYo",
+  authDomain: "hibrida-b0d1f.firebaseapp.com",
+  projectId: "hibrida-b0d1f",
+  storageBucket: "hibrida-b0d1f.firebasestorage.app",
+  messagingSenderId: "40712928402",
+  appId: "1:40712928402:web:723485c0f2c0ed5ab83a4b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
